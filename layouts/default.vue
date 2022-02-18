559d6ff2a5af473507960b5edf807c3f9585784f
Kenan Kadic <kkadic@midtownhi.com>
2022-02-18 16:21:59 -0600

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+<v-main>
+  <Nuxt />
+</v-main>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Login Form'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container>
+    <h1>Trello Clone</h1>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
